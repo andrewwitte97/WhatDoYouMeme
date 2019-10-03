bec00d16789d57cc0a1a5244ceb9a7fed7f74bd1
Andrew Witte <wittea91@my.sunyulster.edu>
2019-10-03 16:50:02 -0400

    more .use function

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const userController = require('./controllers/Users');
 
 const app = express();
 const port = 3000;
 
 app
+    .use( function( req, res, next) {
+        //Logging
+        
+        console.log( {params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers });
+        next();
+    })
+
     .use('/static', express.static(path.join(__dirname, '../NoFramework')))
     
     .get(  '/', 
@@ -12,8 +20,16 @@ app
         )
     .get(  '/heb', 
         function(req, res){
-            res.send('Shalom World!')
+            res.send({ msg: 'Shalom World!' })
         }
-    );
+    )
+    .get(   '/green',
+        function(req, res){
+            res.send('Green World!')
+        })
+
+    .use ('/users', userController );    
+
+
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
---
+++ server/models/Users.js
@@ -0,0 +1,6 @@
+const users = [
+    { name: "Andrew", age: "22"},
+    { name: "Donald", age: "98"}
+];
+
+module.exports = users;
